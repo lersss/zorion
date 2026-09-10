ff5b4eb8bca7952c17eeda08217982ab7ad4e407
lersss <skycomposer@gmail.com>
2026-09-10 23:49:12 +0700

    chore: добавить JWT_SECRET в .env.example и run.ps1, .gitignore в UTF-8

--- run.ps1
+++ run.ps1
@@ -7,6 +7,7 @@ $env:DATABASE_URL = "postgres://zorion:zorion123@127.0.0.1:5432/zorion?sslmode=d
 $env:REDIS_URL = "redis://localhost:6379/0"
 $env:TICK_INTERVAL_SEC = "3"
 $env:ADMIN_PASSWORD = "admin123"
+$env:JWT_SECRET = "dev-secret-change-me-0123456789abcdef0123456789abcdef"
 
 $env:PATH = [System.Environment]::GetEnvironmentVariable("PATH", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("PATH", "User")
 
